eff622ee7a891f590aa0b6fb0e1d6d983d5c4bbd
maulik225 <msompura00@gmail.com>
2020-10-11 20:47:37 +0530

    Module_imported

--- Crudapi/RESTApi/views.py
+++ Crudapi/RESTApi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Employees,Inventory
+from .serializers import EmployeesSerializer,InventorySerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+
