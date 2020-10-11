b31be81781e6dcaf3ce47ed6ac38865ce34f5c14
maulik225 <msompura00@gmail.com>
2020-10-11 20:40:56 +0530

    Installed_apps_added_rest_framework

--- Crudapi/Crudapi/settings.py
+++ Crudapi/Crudapi/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RESTApi'
+    'RESTApi',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
